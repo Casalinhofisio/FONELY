f9575a0556777a646ff7eb96bb269f49ee10ee2a
Casalinhofisio <eduardocasalinho126@gmail.com>
2026-09-23 23:53:45 -0300

    fix: CAA rápida e pictogramas de comunicação melhores
    * fix: publish CAA through fast server endpoint
    
    * feat: upgrade standard CAA cards to ARASAAC pictograms
    
    * style: remove white image boxes and strengthen AAC background
    
    * chore: load new CAA visual client
    
    * chore: replace stale public CAA cache
    
    * chore: load fast CAA publisher
    
    * chore: refresh Fonely loader

--- caa-sw.js
+++ caa-sw.js
@@ -1,5 +1,5 @@
-const CACHE='fonely-caa-shell-v7';
-const SHELL=['./caa.html','./caa-public.css?v=6','./caa-public.js?v=9','./fonely-caa-library.js?v=5','./fonely-caa-speech.js?v=3','./fonely-icon.svg','./caa-manifest.webmanifest'];
+const CACHE='fonely-caa-shell-v8';
+const SHELL=['./caa.html','./caa-public.css?v=7','./caa-public.js?v=10','./fonely-caa-library.js?v=5','./fonely-caa-speech.js?v=3','./fonely-icon.svg','./caa-manifest.webmanifest'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('fonely-caa-shell-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{
