a16d0b4a67ee70bf319a89a03767cb49241b4124
Casalinhofisio <eduardocasalinho126@gmail.com>
2026-09-23 21:17:28 -0300

    chore: refresh offline CAA v3 cache

--- caa-sw.js
+++ caa-sw.js
@@ -1,5 +1,5 @@
-const CACHE='fonely-caa-shell-v2';
-const SHELL=['./caa.html','./caa-public.css?v=3','./caa-public.js?v=4','./fonely-caa-library.js?v=2','./fonely-caa-speech.js?v=2','./fonely-icon.svg','./caa-manifest.webmanifest'];
+const CACHE='fonely-caa-shell-v3';
+const SHELL=['./caa.html','./caa-public.css?v=4','./caa-public.js?v=5','./fonely-caa-library.js?v=3','./fonely-caa-speech.js?v=3','./fonely-icon.svg','./caa-manifest.webmanifest'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('fonely-caa-shell-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{
